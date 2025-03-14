a13a62c9518065214bfaafbdf15da9539f59bf4f
Sharmake <sharmake@MacBookPro.hsd1.ga.comcast.net>
2025-03-14 18:08:44 -0400

    added packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:async';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => GameProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
